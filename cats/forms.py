c0906659cfd2eea343096c345e1c0b7b13047536
p-wel <wojtowiczp97@gmail.com>
2022-08-08 01:21:02 +0200

    FBV converted into CBV

--- cats/forms.py
+++ cats/forms.py
@@ -14,6 +14,11 @@ from cats.models import Rental
 class RentalForm(forms.ModelForm):
     """Form used to create a new Rental object"""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['rental_date'].initial = datetime.date.today()
+        self.fields['return_date'].initial = datetime.date.today()
+
     class Meta:
         model = Rental
         fields = ["cat", "user", "rental_date", "return_date"]
@@ -25,8 +30,10 @@ class RentalForm(forms.ModelForm):
                     "class": "datepicker",
                     "type": "date",
                     "placeholder": "DD-MM-YYYY",
+                    "initial": datetime.date.today(),
                 }
             ),
+
             "return_date": forms.DateInput(
                 attrs={
                     "class": "datepicker",
@@ -51,6 +58,7 @@ class SearchForm(forms.Form):
         widget=forms.DateInput(
             attrs={"class": "datepicker", "type": "date", "placeholder": "DD-MM-YYYY"}
         ),
+        initial=datetime.date.today(),
         required=True,
     )
 
--- cats/views.py
+++ cats/views.py
@@ -8,11 +8,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template.loader import render_to_string
 from django.urls import reverse
+from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, FormMixin
+from django.views.generic.list import MultipleObjectMixin
 
 from cats.forms import RentalForm, SearchForm
 from cats.models import Cat, Species, Breed, Rental
@@ -37,64 +40,51 @@ class SpeciesListView(ListView):
     template_name = "cats/species.html"
 
 
-class ExploreListView(ListView):
+class ExploreFormView(FormView):
+    model = Cat
+    queryset = Cat.objects.order_by('-name')
+    template_name = 'cats/explore_list.html'
+    form_class = SearchForm
 
-    def get(self, request, **kwargs):
-        cats = None
-        page_obj = None
-        form = SearchForm(request.GET or None)
-
-        """
-        If form is valid, show list.
-        If form is not valid, SearchForm will show proper hint
-        """
-        if form.is_valid():
-            date_from = form.cleaned_data["date_from"]
-            date_to = form.cleaned_data["date_to"]
-            cats = Cat.objects.get_available_cats(date_from, date_to)
-
-            paginator = Paginator(cats, 10)
-            page_number = request.GET.get("page")
-            page_obj = paginator.get_page(page_number)
-
-        context = {"cats": cats, "form": form, "page_obj": page_obj}
-        return render(request, "cats/explore_list.html", context)
+    def form_valid(self, form):
+        context = super().get_context_data()
+        date_from = form.cleaned_data["date_from"]
+        date_to = form.cleaned_data["date_to"]
+        cats_filtered = self.model.objects.get_available_cats(date_from, date_to)
+        context['cats_filtered'] = cats_filtered
+        return render(self.request, self.template_name, context)
 
 
-class CatListView(ListView):
-    """Lists Cats from only one Species"""
+class CatFormView(FormView):
+    model = Cat
+    template_name = 'cats/cat_list.html'
+    form_class = SearchForm
 
-    def get(self, request, **kwargs):
-        cats = None
-        page_obj = None
-        form = SearchForm(request.GET or None)
-        breeds = Breed.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-        # If such species id do not exist, then make species=None (html will show proper hint then)
+        # Try to get chosen species_id from database. If it won't be found, html will show proper hint
         try:
             species = Species.objects.get(id=self.kwargs['species_id'])
         except Species.DoesNotExist:
             species = None
+        context['species'] = species
 
-        # If form is valid, show list. If not, SearchForm will show proper hint
-        if form.is_valid():
-            date_from = form.cleaned_data["date_from"]
-            date_to = form.cleaned_data["date_to"]
-            species_cats = Cat.objects.filter(breed__species=species)
-            cats = species_cats.get_available_cats(date_from, date_to)
+        return context
 
-            paginator = Paginator(cats, 10)
-            page_number = request.GET.get("page")
-            page_obj = paginator.get_page(page_number)
+    def form_valid(self, form):
+        context = super().get_context_data()
+        date_from = form.cleaned_data["date_from"]
+        date_to = form.cleaned_data["date_to"]
 
-        context = {
-            "cats": cats,
-            "breeds_list": breeds,
-            "form": form,
-            "species": species,
-            "page_obj": page_obj,
-        }
-        return render(request, "cats/cat_list.html", context)
+        # Filter out only cats with chosen species_id
+        species = Species.objects.get(id=self.kwargs['species_id'])
+        species_cats = Cat.objects.filter(breed__species=species)
+        cats_filtered = species_cats.get_available_cats(date_from, date_to)
+
+        context['species'] = species
+        context['cats_filtered'] = cats_filtered
+        return render(self.request, self.template_name, context)
 
 
 class CatDetailView(DetailView):
@@ -104,7 +94,7 @@ class CatDetailView(DetailView):
     template_name = "cats/details.html"
 
 
-class RentalFormView(FormView, LoginRequiredMixin):
+class RentalFormView(FormView):
     """
     View to let user Rent a Cat, picking proper dates from a RentalForm.
 
@@ -115,52 +105,48 @@ class RentalFormView(FormView, LoginRequiredMixin):
     is directly in Rental model (works every time, even from admin).
     """
 
-    def get(self, request, **kwargs):
+    model = Rental
+    template_name = 'cats/rental_form.html'
+    form_class = RentalForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         cat = get_object_or_404(Cat, pk=self.kwargs['cat_id'])
-        rental_form = RentalForm(
-            initial={"cat": cat, "user": request.user}, data=request.POST or None
+        form = RentalForm(
+            initial={"cat": cat, "user": self.request.user}, data=self.request.POST or None
         )
-        context = {"cat": cat, "rental_form": rental_form}
-        return render(request, "cats/rental_form.html", context)
+        context['cat'] = cat
+        context['form'] = form
+        return context
 
-    def post(self, request, **kwargs):
+    def form_valid(self, form):
         cat = get_object_or_404(Cat, pk=self.kwargs['cat_id'])
-        rental_form = RentalForm(
-            initial={"cat": cat, "user": request.user}, data=request.POST or None
-        )
-        if request.POST:
-            if rental_form.is_valid():
-                rental_form.save()
-                return redirect(reverse("cats:congrats_mail", args=[self.kwargs['cat_id']]))
-
-        context = {"cat": cat, "rental_form": rental_form}
-        return render(request, "cats/rental_form.html", context)
+        form.initial = {"cat": cat, "user": self.request.user}
+        form.data = self.request.POST or None
+        form.save()
+        return redirect(reverse("cats:congrats_mail", args=[self.kwargs['cat_id']]))
 
 
-class RentalListView(ListView, LoginRequiredMixin):
+class RentalListView(LoginRequiredMixin, ListView):
     """View to show all user's rentals"""
 
-    def get(self, request, **kwargs):
-        user_rentals = Rental.objects.filter(user=request.user).order_by("-rental_date")
-        today = datetime.date.today()
+    model = Rental
+    template_name = 'cats/rental_list.html'
 
-        paginator = Paginator(user_rentals, 10)
-        page_number = request.GET.get("page")
-        page_obj = paginator.get_page(page_number)
-
-        context = {"user_rentals": user_rentals, "today": today, "page_obj": page_obj}
-
-        return render(request, "cats/rental_list.html", context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_rentals = Rental.objects.filter(user=self.request.user).order_by('-rental_date')
+        context['user_rentals'] = user_rentals
+        return context
 
 
 @login_required
-def congrats_mail(request, cat_id):
+def rental_congrats_view(request, cat_id):
     """View to send confirmation mail to the user and show him congrats info"""
     cat = get_object_or_404(Cat, pk=cat_id)
     congrats_template = render_to_string(
         "cats/congrats_mail_template.html", {"cat": cat}
     )
-
     send_mail(
         "Congrats, cat rented!",
         congrats_template,
@@ -168,5 +154,4 @@ def congrats_mail(request, cat_id):
         [request.user.email],
         fail_silently=False,
     )
-
     return render(request, "cats/congrats.html", {"cat": cat})
